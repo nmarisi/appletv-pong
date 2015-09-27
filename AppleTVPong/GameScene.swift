a0df30fe1b0ef7098e76f6370aab344da6b7a8a5
Nahuel Marisi <nahuelmarisi@gmail.com>
2015-09-28 00:54:41 +0100

    It's not possible to move both player controllers and bounce off the ball

--- AppleTVPong/GameScene.swift
+++ AppleTVPong/GameScene.swift
@@ -9,19 +9,64 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var player1: SKSpriteNode!
+    var player2: SKSpriteNode!
+    var ball: SKSpriteNode!
+    var initialTouch:CGPoint!
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
+        
+        initialTouch = view.frame.origin
+        player1 = self.childNodeWithName("Player1") as! SKSpriteNode
+        player2 = self.childNodeWithName("Player2") as! SKSpriteNode
+        ball = self.childNodeWithName("Ball") as! SKSpriteNode
 
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         /* Called when a touch begins */
-        
-        for touch in touches {
+       
+        guard let touch = touches.first else {
+            return
         }
+        
+        initialTouch = touch.locationInNode(self)
+   
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let touchLocation = touch.locationInNode(self)
+        
+        if touchLocation.y < initialTouch.y { // Going down
+            let moveAction = SKAction.moveBy(CGVectorMake(0, -20), duration: 0.5)
+            player1.runAction(moveAction)
+            player2.runAction(moveAction)
+        } else { // Going up
+            //let moveAction = SKAction.moveToY(touchLocation.y, duration: 0.5)
+            let moveAction = SKAction.moveBy(CGVectorMake(0, +20), duration: 0.5)
+            player1.runAction(moveAction)
+            player2.runAction(moveAction)
+        }
+        
+        print(touchLocation)
+        
+    
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+       
+        
+        
     }
    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        
+        
     }
 }
