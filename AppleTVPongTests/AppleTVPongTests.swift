b5c904d250331f46f2e902dd7dc24230335e9eff
Nahuel Marisi <nahuelmarisi@gmail.com>
2015-09-27 17:32:45 +0100

    Initial Commit

---
+++ AppleTVPongTests/AppleTVPongTests.swift
@@ -0,0 +1,36 @@
+//
+//  AppleTVPongTests.swift
+//  AppleTVPongTests
+//
+//  Created by Nahuel Marisi on 2015-09-27.
+//  Copyright © 2015 TechBrewers. All rights reserved.
+//
+
+import XCTest
+@testable import AppleTVPong
+
+class AppleTVPongTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
