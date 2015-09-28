c111ee27e5c252e1626a405f7c83d62419e63f25
Nahuel Marisi <nahuel@milocreative.com>
2015-09-28 15:33:21 +0100

    fine tuned player control and cleaned up code to a separate method

--- AppleTVPong/GameScene.swift
+++ AppleTVPong/GameScene.swift
@@ -15,45 +15,55 @@ class GameScene: SKScene {
     var ball: SKSpriteNode!
     var initialTouch:CGPoint!
     
+    let boundaryWith:CGFloat = 40
+    
     override func didMoveToView(view: SKView) {
         
         initialTouch = view.frame.origin
         player1 = self.childNodeWithName("Player1") as! SKSpriteNode
         player2 = self.childNodeWithName("Player2") as! SKSpriteNode
         ball = self.childNodeWithName("Ball") as! SKSpriteNode
+        
+        //ball.physicsBody!.applyImpulse(CGVectorMake(1000, -10))
+        ball.physicsBody?.velocity = CGVectorMake(500, 250)
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        
+        
+        let borderBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        borderBody.usesPreciseCollisionDetection = true
+        borderBody.friction = 0
+        self.physicsBody = borderBody
+        
+        
 
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
         /* Called when a touch begins */
-       
+        
         guard let touch = touches.first else {
             return
         }
+        
+        
         
         initialTouch = touch.locationInNode(self)
    
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+
         guard let touch = touches.first else {
             return
         }
-        
         let touchLocation = touch.locationInNode(self)
+        let previousLocation = touch.previousLocationInNode(self)
         
-        if touchLocation.y < initialTouch.y { // Going down
-            let moveAction = SKAction.moveBy(CGVectorMake(0, -20), duration: 0.5)
-            player1.runAction(moveAction)
-            player2.runAction(moveAction)
-        } else { // Going up
-            //let moveAction = SKAction.moveToY(touchLocation.y, duration: 0.5)
-            let moveAction = SKAction.moveBy(CGVectorMake(0, +20), duration: 0.5)
-            player1.runAction(moveAction)
-            player2.runAction(moveAction)
-        }
+        let player1Y = player1.position.y + (touchLocation.y - previousLocation.y)
+        movePlayer(player1, yLocation: player1Y)
         
-        print(touchLocation)
+
         
     
     }
@@ -69,4 +79,19 @@ class GameScene: SKScene {
         
         
     }
+    
+    //MARK: Convenience functions
+    func movePlayer(player: SKSpriteNode, yLocation: CGFloat) {
+       
+        // Avoid moving the player outside of the screen
+        if yLocation > (view!.frame.size.width / 2) + (player1.size.width  / 2) ||
+            yLocation < boundaryWith + (player1.size.width / 2) {
+            return
+        }
+        print(yLocation)
+
+        player1.position = CGPointMake(player.position.x, yLocation)
+        
+    }
+    
 }
