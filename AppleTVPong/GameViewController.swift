3a67b08621b603920c49fa1baed916ce34f29b0b
Nahuel Marisi <nahuelmarisi@gmail.com>
2015-10-25 17:03:06 +0100

    removed hardcoded scene name

--- AppleTVPong/GameViewController.swift
+++ AppleTVPong/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed: "GameScene") {
+        if let scene = GameScene(fileNamed: Constants.Scenes.GameScene) {
             // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = true
